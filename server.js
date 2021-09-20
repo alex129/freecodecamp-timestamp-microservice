532d2d0146aa422a3fda9e4516cfbc38b96f6e8e
alejandro <AlejP2121>
2021-09-20 12:51:31 +0200

    timestamp project finished

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,22 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+const router = express.Router()
+router.get('/:date', (req, res) => {
+  let date = new Date(req.params.date);
+  if (date) {
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+    res.json({ unix: unix, utc: utc });
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
+});
+app.use('/api', router);
+
 
 
 
